b169a9f542d97d9ac3fc77260e9e425f307343d2
monalisa180 <monanayak268@gmail.com>
2025-12-17 17:48:38 +0530

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,17 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container">
+    <a class="navbar-brand">Student App</a>
+
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#menu">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="menu">
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item"><a class="nav-link" href="dashboard.php">Dashboard</a></li>
+        <li class="nav-item"><a class="nav-link" href="auth/profile.php">Profile</a></li>
+        <li class="nav-item"><a class="nav-link" href="logout.php">Logout</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
---
+++ profile.php
@@ -0,0 +1,43 @@
+<?php
+session_start();
+include "config.php";
+
+if (!isset($_SESSION['user_id'])) {
+    header("Location: login.php");
+    exit();
+}
+
+$user_id = $_SESSION['user_id'];
+
+$result = mysqli_query($conn, "SELECT * FROM users WHERE id='$user_id'");
+$user = mysqli_fetch_assoc($result);
+?>
+
+
+<!DOCTYPE html>
+<html>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+<head>
+<title>Profile</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+
+<div class="container mt-5">
+  <div class="card">
+    <div class="card-header bg-dark text-white">
+      Profile
+    </div>
+    <div class="card-body">
+      <p><strong>Name:</strong> <?php echo $user['username']; ?></p>
+      <p><strong>Email:</strong> <?php echo $user['email']; ?></p>
+
+      <a href="dashboard.php" class="btn btn-primary">Back</a>
+    </div>
+  </div>
+</div>
+
+</body>
+</html>
