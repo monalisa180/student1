9da991b2c9884943300fe5b1cadf4dfcea2cd27b
monalisa180 <monanayak268@gmail.com>
2025-12-17 18:47:36 +0530

    Update logout.php

--- logout.php
+++ logout.php
@@ -2,4 +2,7 @@
 session_start();
 session_destroy();
 header("Location: login.php");
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+
 ?>
+<meta name="viewport" content="width=device-width, initial-scale=1">
